7b88d0773942b0fdb1c54be33d7335804f1e617d
BuildTools <unconfigured@null.spigotmc.org>
2019-01-31 23:56:49 +0900

    Fully working 520A code

--- Post 2019/520A/Pangram.java
+++ Post 2019/520A/Pangram.java
@@ -7,7 +7,11 @@ public class Pangram {
 		int n = scanner.nextInt();
 		String str = scanner.next();
 
-		System.out.println(isPangram(str)?"YES":"NO");
+		if(n<26) {
+			System.out.println("NO");
+		} else {
+			System.out.println(isPangram(str)?"YES":"NO");
+		}
 
 		
 		
@@ -16,7 +20,7 @@ public class Pangram {
 
 	static boolean isPangram(String inp) {
 			int cnt=0;
-			// ascii 65-122 are alphabets, created array for each letter (ex: alph[0] = A)
+			// ascii 65-122 are alphabets, created array for each letter (ex: upperCase[0] = A, lowerCase[0] = a)
 			// each index is initialized to 0
 			int lowerCase[] = new int[26];
 			int upperCase[] = new int[26];
@@ -25,7 +29,7 @@ public class Pangram {
 				upperCase[i] = 0;
 			}
 
-			// checks input string for letters. For each letter found, its respective alph index is set to 1
+			// checks input string for letters. For each letter found, its respective index is set to 1
 			for(int i=0; i<inp.length(); i++) {
 				for(int j=0; j<26; j++) {
 					if((int)inp.charAt(i) == j+65) {
@@ -37,13 +41,15 @@ public class Pangram {
 				}
 			}
 
-			//adds the 
+			//adds the values of each index.
+			//note) A and a only counts as one
 			for(int i=0; i<26; i++) {
-				if(upperCase[i] != lowerCase[i])
-					cnt += upperCase[i] + lowerCase[i];
+				if(upperCase[i] == 1 && lowerCase[i] == 1)
+					cnt += 1;
 				else
-					continue;
-//				System.out.print( (char)(i+65)+": " + alph[i] + ", ");
+					cnt += upperCase[i] + lowerCase[i];
+//				System.out.print( (char)(i+65)+": " + upperCase[i] + ", ");
+//				System.out.print( (char)(i+97)+": " + lowerCase[i] + ", ");
 			}
 //			System.out.print("\n"+cnt+"\n");
 			
