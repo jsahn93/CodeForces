34646523e2bf245e2937297b742706485ff06f79
BuildTools <unconfigured@null.spigotmc.org>
2019-01-31 23:35:02 +0900

    Fixed errors in 520A

--- Post 2019/520A/Pangram.java
+++ Post 2019/520A/Pangram.java
@@ -16,21 +16,33 @@ public class Pangram {
 
 	static boolean isPangram(String inp) {
 			int cnt=0;
-			int alph[] = new int[58];
-			for(int i=0; i<58; i++) {
-				alph[i] = 0;
+			// ascii 65-122 are alphabets, created array for each letter (ex: alph[0] = A)
+			// each index is initialized to 0
+			int lowerCase[] = new int[26];
+			int upperCase[] = new int[26];
+			for(int i=0; i<26; i++) {
+				lowerCase[i] = 0;
+				upperCase[i] = 0;
 			}
 
+			// checks input string for letters. For each letter found, its respective alph index is set to 1
 			for(int i=0; i<inp.length(); i++) {
-				for(int j=0; j<58; j++) {
+				for(int j=0; j<26; j++) {
 					if((int)inp.charAt(i) == j+65) {
-						alph[j] = 1;
+						upperCase[j] = 1;
+					} else if ((int)inp.charAt(i) == j+97){
+						lowerCase[j] = 1;
 					}
+
 				}
 			}
 
-			for(int i=0; i<58; i++) {
-				cnt += alph[i];
+			//adds the 
+			for(int i=0; i<26; i++) {
+				if(upperCase[i] != lowerCase[i])
+					cnt += upperCase[i] + lowerCase[i];
+				else
+					continue;
 //				System.out.print( (char)(i+65)+": " + alph[i] + ", ");
 			}
 //			System.out.print("\n"+cnt+"\n");
