a3b79f454c807e4a2fa886f5103743f01fba438d
BuildTools <unconfigured@null.spigotmc.org>
2019-02-04 22:13:36 +0900

    shortened code of 705A/Hulk.java

--- Post_2019/705A/Hulk.java
+++ Post_2019/705A/Hulk.java
@@ -5,21 +5,8 @@ public class Hulk {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
 
-		if(n==1) {
-				System.out.println("I hate it");
-		} else {
-			StringBuffer sbuffer = new StringBuffer("hate ");
-			for(int i=2; i<=n; i++) {
-				
-
-				sbuffer.append("that I ");
-				if(i%2 == 0 ) {
-					sbuffer.append("love ");
-				} else {
-					sbuffer.append("hate ");
-				}
-			}
-			System.out.println("I " +sbuffer + "it");
+		for(int i=1; i<=n; i++) {
+			System.out.print("I "+(i%2==0?"love":"hate")+" "+(i==n?"it":"that "));
 		}
 
 	}
