97a24ecc88db88ed73b73ded92a6d2d147112392
BuildTools <unconfigured@null.spigotmc.org>
2019-02-05 18:36:38 +0900

    Removed duplicate folders

--- Post 2019/451A/Eight451A.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class Eight451A {
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println(Math.min(sc.nextInt(), sc.nextInt())%2==0?"Malvika":"Akshat");
-	}
-}
